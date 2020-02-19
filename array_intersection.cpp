3f27c830dd31e0812c76809aa812481875256f22
songmeo <shuong1012@gmail.com>
2020-02-19 04:31:40 +0200

    add note

--- array_intersection.cpp
+++ array_intersection.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
+#include <unordered_map>
 
-class Solution {
+class OldSolution {
 public:
+  std::vector<int> result;
   std::vector<int> intersection(std::vector<int> nums1, std::vector<int> nums2) {
-    std::vector<int> result;
     std::sort(nums1.begin(), nums1.end());
     std::sort(nums2.begin(), nums2.end());
     for(int i = 0; i < nums1.size(); ++i) {
@@ -14,13 +16,24 @@ public:
           result.push_back(nums1[i]);
       }
     }
-    result.erase(std::unique( result.begin(), result.end()), result.end());
+    result.erase(std::unique( result.begin(), result.end()), result.end()); //unique returns the pointer to the element next to the last element which is not removed
     return result;  
   }
 };
 
+//this one uses set because set elements are ordered and unique
+class Solution {
+public:
+  std::set<int> s1,s2;
+  std::vector<int> intersection(std::vector<int> n1, std::vector<int> n2) {
+
+        
+  }
+};
+
+
 int main() {
-  Solution s;
+  OldSolution s;
   //std::vector s1{4,9,5};
   //std::vector s2{9,4,9,8,4};
   for(int i : s.intersection({4,9,5},{9,4,9,8,4})) {
