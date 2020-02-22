fe5b4bd49c525aad7ec3dbc459e4aef3be9bfa74
songmeo <shuong1012@gmail.com>
2020-02-22 16:04:22 +0200

    first commit

---
+++ divisible_subarrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int disvisibleSubarrays(vector<int>& v, int k) {
+  
+  }    
+};
+
+int main() {
+  vector<int> v{4,5,0,-2,-3,1};
+  cout << Solution.divisibleSubarrays(v,5);
+  return 0;
+}
