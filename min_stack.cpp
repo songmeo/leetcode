8966ea3e5f0e9801be12306285ecda02b71c861f
songmeo <shuong1012@gmail.com>
2020-02-23 22:08:07 +0200

    min stack and two sum

---
+++ min_stack.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class MinStack {
+public:
+  MinStack() {
+        
+  }
+    
+  void push(int x) {
+        
+  }
+    
+  void pop() {
+        
+  }
+    
+  int top() {
+        
+  }
+    
+  int getMin() {
+        
+  }
+};
+
+int main() {
+  MinStack ms = new MinStack();
+
+  return 0;
+}
