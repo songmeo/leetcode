e4bea90b60c255bcccc6dbc5312444c3ec76951e
songmeo <shuong1012@gmail.com>
2020-02-24 15:15:11 +0200

    deque

--- min_stack.cpp
+++ min_stack.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
+#include <deque>
+using namespace std;
 
+//deque is short for double ended queue
 class MinStack {
 public:
+  deque<int> myStack{};
   MinStack() {
         
   }
     
   void push(int x) {
-        
+    myStack.push_back(x);
   }
     
   void pop() {
-        
+    myStack.pop_back();
   }
     
   int top() {
-        
+    return myStack.back();
   }
     
   int getMin() {
-        
+    return 0;      
   }
 };
 
 int main() {
-  MinStack ms = new MinStack();
-
+  MinStack ms;
+  ms.push(1);
+  cout << ms.top();
   return 0;
 }
