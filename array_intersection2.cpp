88816490a33a551e37db60d6f0c4fcfeb6ca9b77
songmeo <shuong1012@gmail.com>
2020-02-24 11:05:48 +0200

    first try

--- array_intersection2.cpp
+++ array_intersection2.cpp
@@ -8,7 +8,24 @@ class Solution {
 public:
   vector<int> result;
   vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-    
+    unordered_map<int,bool> m;
+    for(int i : nums1) {
+      m[i] = false;
+    }
+    for(int i : nums2) {
+      if(m.count(i) && !m[i]) {
+        m[i] = true;
+        result.push_back(i);
+      }
+    }
     return result;
   }
+};
+
+int main() {
+  Solution s;
+  vector<int> v1{1,9,4};
+  vector<int> v2{9,4};
+  for(int i : s.intersect(v1,v2)) cout << i << " ";
+  return 0;
 }
