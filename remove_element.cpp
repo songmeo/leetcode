19053042bf9acc5e05692c61c2afcc83d2fd8947
songmeo <shuong1012@gmail.com>
2020-02-25 09:29:56 +0200

    remove element

---
+++ remove_element.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int result;
+  int removeElement(vector<int>& nums, int val) {
+    
+  }
+};
+
+int main() {
+  Solution s;
+  vector<int> v{3,2,2,3};
+  cout << s.removeElement(v);
+  return 0;  
+}
