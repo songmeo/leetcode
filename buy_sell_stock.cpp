959eae63a6e34e3f9d946a2b9faab85f74425690
songmeo <shuong1012@gmail.com>
2020-02-25 18:45:30 +0200

    delete comment

--- buy_sell_stock.cpp
+++ buy_sell_stock.cpp
@@ -6,12 +6,6 @@ using namespace std;
 class Solution {
 public:
   int maxProfit(vector<int>& prices) {
-    /*
-    int min_p = *min_element(prices.begin(), prices.end());
-    int max_p = *max_element(prices.begin(), prices.end());
-    int max_s = *max_element(min_element(prices.begin(),prices.end()), prices.end());
-    int min_s = *min_element(prices.begin(), max_element(prices.begin(), prices.end()));
-    */
     auto min = min_element(prices.begin(),prices.end());
     auto max = max_element(prices.begin(),prices.end());
     int d1 = *max_element(min, prices.end()) - *min;
