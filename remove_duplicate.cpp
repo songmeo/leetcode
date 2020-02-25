4cfc23c45c834bb026bc19c4b784ee99e59bb34f
songmeo <shuong1012@gmail.com>
2020-02-25 09:07:46 +0200

    got runtime error on server but worked in my computer

--- remove_duplicate.cpp
+++ remove_duplicate.cpp
@@ -11,6 +11,7 @@ public:
     for(auto it1 = nums.begin(); it1 != nums.end(); ++it1) {
       for(auto it2 = it1 + 1; it2 != nums.end() && *it2 == *it1; ++it2) {
         nums.erase(it2);
+        --it1;
         --result;
       }
     }
