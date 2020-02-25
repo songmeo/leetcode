aff8afcd2abcf05b1728e649e8e934789942b90f
songmeo <shuong1012@gmail.com>
2020-02-25 21:25:45 +0200

    buy sell stock 2

---
+++ buy_sell_stock2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int maxProfit(vector<int>& v) {
+  
+  }
+}
+
+int main() {
+  Solution s;
+  vector<int> v{7,1,5,3,6,4};
+  cout << s.maxProfit(v);
+  return 0;
+}
