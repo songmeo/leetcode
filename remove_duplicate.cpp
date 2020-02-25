79de3005234795480e236733499da5507a5eb77a
songmeo <shuong1012@gmail.com>
2020-02-25 09:01:15 +0200

    optimizing

--- remove_duplicate.cpp
+++ remove_duplicate.cpp
@@ -8,10 +8,10 @@ int result;
 public:
   int removeDuplicates(vector<int>& nums) {
     result = nums.size();
-    for(int i = 0; i < nums.size(); ++i) {
-      for(auto it = nums.begin() + i + 1; it != nums.end() && *it == nums[i]; ++it) {
-        nums.erase(it);
-        result--;
+    for(auto it1 = nums.begin(); it1 != nums.end(); ++it1) {
+      for(auto it2 = it1 + 1; it2 != nums.end() && *it2 == *it1; ++it2) {
+        nums.erase(it2);
+        --result;
       }
     }
     return result;
@@ -20,7 +20,7 @@ public:
 
 int main() {
   Solution s;
-  vector<int> nums{0,0,1,2};
+  vector<int> nums{0,0,1,1,1,2,2,3,3,4};
   cout << s.removeDuplicates(nums);
   return 0;
 }
