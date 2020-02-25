91ec9b9e7585cf580839a87cfcc5f87c4e5b59d8
songmeo <shuong1012@gmail.com>
2020-02-25 18:45:03 +0200

    optimizing

--- buy_sell_stock.cpp
+++ buy_sell_stock.cpp
@@ -6,17 +6,23 @@ using namespace std;
 class Solution {
 public:
   int maxProfit(vector<int>& prices) {
+    /*
     int min_p = *min_element(prices.begin(), prices.end());
     int max_p = *max_element(prices.begin(), prices.end());
     int max_s = *max_element(min_element(prices.begin(),prices.end()), prices.end());
     int min_s = *min_element(prices.begin(), max_element(prices.begin(), prices.end()));
-    return (max_s - min_p > max_p - min_s)?(max_s - min_p):(max_p - min_s);
+    */
+    auto min = min_element(prices.begin(),prices.end());
+    auto max = max_element(prices.begin(),prices.end());
+    int d1 = *max_element(min, prices.end()) - *min;
+    int d2 = *max - *min_element(prices.begin(), max);
+    return (d1 > d2)?d1:d2;
   }
 };
 
 int main() {
   Solution s; 
-  vector<int> v{7,1,5,3,6,4};
+  vector<int> v{7,6,4,3,1};
   cout << s.maxProfit(v);
   return 0;
 }
