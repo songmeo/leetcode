5d297efccdd550dc7d57782e945ee7f4739e3d45
songmeo <shuong1012@gmail.com>
2020-02-26 10:46:41 +0200

    using binary search

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-class Solution {
+//using std algorithm
+class Solution1 {
 public:
   bool containsDuplicate(vector<int>& nums) {
     int s1 = nums.size();
@@ -15,8 +16,39 @@ public:
   }
 };
 
+//use binarySearch
+class Solution {
+public:
+  int binarySearch(vector<int>& v, int k) {
+    int max = v.size() - 1;
+    int min = 0;
+    int mid;
+    while(max >= min) {
+      mid = (max + min)/2;
+      if(v[mid] > k)
+        max = mid - 1;
+      else if(v[mid] < k)
+        min = mid + 1;
+      else
+        return mid; 
+    }
+    return -1;
+  }
+  
+  bool containsDuplicate(vector<int>& v) {
+    sort(v.begin(), v.end());
+    for(int i = 0; i < v.size() - 1; ++i) {
+      vector<int> tmp(v.begin() + i + 1, v.end());
+      if(binarySearch(tmp,v[i]) != -1) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
 int main() {
-  vector<int> v{2,3,1};
+  vector<int> v{1,2,3,1};
   Solution s;
   cout << s.containsDuplicate(v);
   return 0;
