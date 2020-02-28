d02fa6c4349efe3eb82ccf61377661f29133fd63
songmeo <shuong1012@gmail.com>
2020-02-28 12:10:40 +0200

    worker

---
+++ others/workers.cpp
@@ -0,0 +1,13 @@
+/******************************************************************************
+ 
+there are N <= 5000 workers. Each is available during some days of the months.
+Find 2 workers with maximum intersection of their schedules. 
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
