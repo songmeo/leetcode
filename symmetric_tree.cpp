e252bb3cb57fe647d884884643b4cf1554c8e39f
songmeo <shuong1012@gmail.com>
2020-02-29 12:35:56 +0200

    finished

--- symmetric_tree.cpp
+++ symmetric_tree.cpp
@@ -21,14 +21,22 @@ TreeNode* insert(int arr[], TreeNode* root, int i, int n) {
 class Solution {
 public:
   bool isSymmetric(TreeNode* root) {
-    return true;
+	if(!root) return true;
+	return isSymmetric(root->left,root->right);
+  }
+  
+  bool isSymmetric(TreeNode* left, TreeNode* right) {
+	if(!left || !right)
+	  return left == right;
+	if(left->val != right->val) return false;
+	return isSymmetric(left->left,right->right) && isSymmetric(left->right,right->left);
   }
 };
 
 
 int main() {
   Solution s;
-  int arr[] = {1,2,2,3,4,4,3};
+  int arr[] = {1,2,2,3,4,4};
   TreeNode* root = insert(arr,root,0,sizeof(arr)/sizeof(arr[0]));
   cout << s.isSymmetric(root);
   return 0;
