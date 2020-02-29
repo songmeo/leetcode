917061b4e3ccb5a4b2792fd6ae6052d36e11c110
songmeo <shuong1012@gmail.com>
2020-02-29 16:10:34 +0200

    add iterative solution

--- symmetric_tree.cpp
+++ symmetric_tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 struct TreeNode {
@@ -18,7 +19,8 @@ TreeNode* insert(int arr[], TreeNode* root, int i, int n) {
   return root;
 }
 
-class Solution {
+//recursive way
+class Solution1 {
 public:
   bool isSymmetric(TreeNode* root) {
 	if(!root) return true;
@@ -33,10 +35,29 @@ public:
   }
 };
 
+//iterative way
+class Solution {
+  public:
+	  bool isSymmetric(TreeNode* root) {
+		queue<TreeNode*> q;
+		q.push(root);
+		q.push(root);
+		while(!q.empty()) {
+		  TreeNode* t1 = q.front();
+		  q.pop();
+		  TreeNode* t2 = q.front();
+		  q.pop();
+		  if(!t1 && !t2) continue;
+		  if(!t1 || !t2 || t1->val != t2->val) return false;
+		}
+		return true;
+	  }
+};
+
 
 int main() {
   Solution s;
-  int arr[] = {1,2,2,3,4,4};
+  int arr[] = {1,2,2,3,4,4,3};
   TreeNode* root = insert(arr,root,0,sizeof(arr)/sizeof(arr[0]));
   cout << s.isSymmetric(root);
   return 0;
