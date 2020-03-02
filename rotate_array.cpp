5e7437f48f0f67ed640c0a1a38c73ef9e2eef198
songmeo <shuong1012@gmail.com>
2020-03-02 04:28:35 +0200

    first way

--- rotate_array.cpp
+++ rotate_array.cpp
@@ -3,18 +3,30 @@
 #include <algorithm>
 using namespace std;
 
-//using library
+/*
+using library doesn't work for cases when nums size is smaller than k
 class Solution1 {
 public:
-  void Rotate(vector<int>& nums, int k) {
-    rotate(nums.begin(),nums.end() - k, nums.end()); 
+  void rotate(vector<int>& nums, int k) {
+    std::rotate(nums.begin(),nums.end() - k, nums.end()); 
+  }
+};
+*/
+
+class Solution {
+public:
+  void rotate(vector<int>& nums, int k) {
+    while(k--) {
+	  nums.insert(nums.begin(), *(nums.end() - 1));
+	  nums.pop_back();
+	}
   }
 };
 
 int main() {
-  Solution1 s;
-  vector<int> v{1,2,3,4,5,6,7};
-  s.Rotate(v, 3);
+  Solution* s = new Solution();
+  vector<int> v{1,2,3,4,5,6};
+  s->rotate(v, 3);
   for(int i : v) cout << i << " ";
   return 0; 
 }
