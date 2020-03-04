56146c764c88fbe7e376aa8186a03cea2579193e
songmeo <shuong1012@gmail.com>
2020-03-04 02:02:11 +0200

    when string is empty

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -5,18 +5,21 @@ using namespace std;
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        for(auto it1 = s.begin(), it2 = s.end() - 1; it1 != s.begin() + s.size()/2; ++it1, --it2) {
-            int tmp = move(*it1);
-            *it1 = move(*it2);
-            *it2 = move(tmp);
-        }
+		if(!s.empty()) {
+			for(auto it1 = s.begin(), it2 = s.end() - 1; it1 != s.begin() + s.size()/2 && s.size() > 0; ++it1, --it2) {
+				int tmp = move(*it1);
+				*it1 = move(*it2);
+				*it2 = move(tmp);
+			}
+		}
     }
 };
 
 int main() {
 	Solution s;
 	vector<char> str{'h','e','l','l','o'};
-	s.reverseString(str);
-	for(char i : str) cout << i;
+	vector<char> s1;
+	s.reverseString(s1);
+	for(char i : s1) cout << i;
 	return 0;
 }
