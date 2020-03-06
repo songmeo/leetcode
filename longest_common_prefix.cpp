a434320e6f7b287f5ad130885d675fb866096654
songmeo <shuong1012@gmail.com>
2020-03-06 20:33:11 +0200

    recursive, divide and conquer

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 //naive way
-class Solution {
+class Solution1 {
 public:
 	string commonPrefix(string a, string b) {
-		int i;
+		size_t i;
 		for(i = 0; i < a.length() && a[i] == b[i]; i++);
 		return a.substr(0,i);
 	}
@@ -19,11 +19,31 @@ public:
 			if(s.empty()) return "";
 		}
 		string cp = commonPrefix(strs[0],strs[1]);
-		for(int i = 2; i < strs.size(); i++) {
+		for(size_t i = 2; i < strs.size(); i++) {
 			cp = commonPrefix(cp, strs[i]);
 			cout << cp;
 		}
 		return cp;
+	}
+};
+
+//divide and conquer
+class Solution {
+public:
+	string commonPrefix(string a, string b) {
+		size_t i;
+		for(i = 0; i < min(a.length(),b.length()) && a[i] == b[i]; i++);
+		return a.substr(0,i);
+	}
+	string longestCommonPrefix(vector<string>& strs) {
+		if(strs.size() == 0) return "";
+		if(strs.size() == 1) return strs[0];
+		int mid = strs.size()/2;
+		vector<string> left(strs.begin(), strs.begin() + mid);
+		vector<string> right(strs.begin() + mid + 1, strs.end());
+		string lcpleft = longestCommonPrefix(left);
+		string lcpright = longestCommonPrefix(right);
+		return commonPrefix(lcpleft, lcpright);
 	}
 };
 
