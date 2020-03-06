d4f66d3bb566dcffcfb00498a3bc6341d02e93e5
songmeo <shuong1012@gmail.com>
2020-03-06 15:56:53 +0200

    init

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+//naive way
+class Solution {
+public:
+	string longestCommonPrefix(vector<string>& strs) {
+		
+	}
+}
+
+int main() {
+	Solution s;
+	vector<string> s = {"flower","flow","flight"};
+	cout << s.longestCommonPrefix(s);
+	return 0;
+}
