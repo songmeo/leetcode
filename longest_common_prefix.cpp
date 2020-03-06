857d283cc58ba9b4ddee236d4f5b6ef720fef37c
songmeo <shuong1012@gmail.com>
2020-03-06 19:46:04 +0200

    naive way

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
+using namespace std;
 //naive way
 class Solution {
 public:
-	string longestCommonPrefix(vector<string>& strs) {
-		
+	string commonPrefix(string a, string b) {
+		int i;
+		for(i = 0; i < a.length() && a[i] == b[i]; i++);
+		return a.substr(0,i);
 	}
-}
+	
+	string longestCommonPrefix(vector<string>& strs) {
+		string cp = commonPrefix(strs[0],strs[1]);
+		for(int i = 2; i < strs.size(); i++) {
+			cp = commonPrefix(cp, strs[i]);
+			cout << cp;
+		}
+		return cp;
+	}
+};
 
 int main() {
 	Solution s;
-	vector<string> s = {"flower","flow","flight"};
-	cout << s.longestCommonPrefix(s);
+	vector<string> strs = {"flower","flow","flight"};
+	cout << s.longestCommonPrefix(strs);
 	return 0;
 }
