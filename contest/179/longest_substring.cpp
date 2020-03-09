4fa185ed93766c4608dbed1087771ec26ec050e0
songmeo <shuong1012@gmail.com>
2020-03-09 23:05:22 +0200

    understood

--- contest/179/longest_substring.cpp
+++ contest/179/longest_substring.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 using namespace std;
 
+/*
 class Solution {
 public:
 	bool evenVowels(string s) {
@@ -27,8 +28,74 @@ public:
 		while(min < max && !evenVowels(s.substr(min,max))) {
 			max--;
 		}
-		return max - min;
+		int r1 = max - min;
+		max = s.length() - 1;
+		while(min < max && !evenVowels(s.substr(min,max))) {
+			min++;class Solution {
+public:
+	int findTheLongestSubstring(string s) {
+		unordered_map<int,int> m{{0,-1}};
+		int res = 0, n = s.length(), cur = 0;
+		for(int i = 0; i < n; i++) {
+			cur ^= 1 << (string("aeiou").find(s[i]) + 1) >> 1;
+			if(!m.count(cur)) m[cur] = i;
+			res = max(res, i - m[cur]);
+		}
+		for(auto p : m) {
+			cout << p.first << " " << p.second << endl;
+		}
+		return res;
+	}
+};
+
+		}
+		int r2 = max - min;
+		return (r1 > r2)?r1:r2;
     }
+};
+
+
+class Solution {
+public:
+	int findTheLongestSubstring(string s) {
+		unordered_map<int, int> m {{
+	}
+}
+*/
+
+/*
+class Solution {
+public:
+	int findTheLongestSubstring(string s) {
+		unordered_map<int,int> m{{0,-1}};
+		int res = 0, n = s.length(), cur = 0;
+		for(int i = 0; i < n; i++) {
+			cur ^= 1 << (string("aeiou").find(s[i]) + 1) >> 1;
+			//cout << cur << endl;
+			if(!m.count(cur)) m[cur] = i;
+			res = max(res, i - m[cur]);
+		}
+		for(auto p : m) {
+			cout << p.first << " " << p.second << endl;
+		}
+		return res;
+	}
+};
+*/
+
+class Solution {
+public:
+	int findTheLongestSubstring(string str) {
+		unordered_map<int, int> m {{0,-1}};
+		int cur = 0, res = 0;
+		
+		for(int i = 0; i < (int) str.length(); ++i) {
+			cur ^= 1 << (string("aeiou").find(i) + 1) >> 1;
+			if(!m.count(cur)) m[cur] = i;
+			res = max(res, i - m[cur]);
+		}
+		return res;
+	}
 };
 
 int main() {
