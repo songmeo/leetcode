9fc1bfd9cd438483df73116a1816817ff3189e77
songmeo <shuong1012@gmail.com>
2020-03-16 17:11:43 +0200

    optimizing condition

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -6,8 +6,8 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
 		if(!s.empty()) {
-			for(auto it1 = s.begin(), it2 = s.end() - 1; it1 != s.begin() + s.size()/2 && s.size() > 0; ++it1, --it2) {
-				int tmp = move(*it1);
+			for(auto it1 = s.begin(), it2 = s.end() - 1; it2 > it1; ++it1, --it2) {
+				char tmp = move(*it1);
 				*it1 = move(*it2);
 				*it2 = move(tmp);
 			}
