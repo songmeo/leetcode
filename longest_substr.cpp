abec4d323ee0dcf74bf07601cb6c7f240f34482a
songmeo <shuong1012@gmail.com>
2020-03-22 09:24:02 +0200

    first testcase

--- longest_substr.cpp
+++ longest_substr.cpp
@@ -3,17 +3,26 @@
  */
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
 	string longestSubstr(string s) {
-	
+		unordered_map<char,int> m;
+		string result = "";
+		for(auto it = s.begin(); it != s.end(); it++) {
+			if(m.find(*it) != m.end()) return result;
+			result += *it;
+			m[*it]++;
+		}
+		return result;
 	}
 };
 
 int main() {
-	string s = "abcabcbb";
-	
+	Solution s;
+	cout << s.longestSubstr("abcabcbb");
 	return 0;
 }
