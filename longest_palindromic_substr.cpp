b49712e27ac35c6ba2ca955e94a9653ddaeab4d3
songmeo <shuong1012@gmail.com>
2020-03-23 22:16:13 +0200

    compiled

--- longest_palindromic_substr.cpp
+++ longest_palindromic_substr.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Solution {
-	string validPalindrome(string s) {
+public:
+	bool validPalindrome(string s) {
 		for(int i = 0, j = s.length() - 1; i < j; i++, j--) {
 			if(s[i] != s[j])
 				return false;
@@ -11,7 +13,16 @@ class Solution {
 		return true;
 	}
 	
-	string longestPalindrome(string s) {
+	string longestPalindrome(string str) {
+		string result;
+		for(string::iterator it = str.begin(); it != str.end(); it++) {
+			string::iterator next = find(it + 1, str.end(), *it);
+			if(next != str.end()) {
+				string tmp = string(it, next + 1);
+				if(validPalindrome(tmp) && tmp.length() > result.length()) result = tmp; 
+			}
+		}
+		return result;
 	}
 };
 
