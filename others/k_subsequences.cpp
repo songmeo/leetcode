a4b538e9887788fbb496c651038933840f204e85
songmeo <shuong1012@gmail.com>
2020-03-24 11:26:21 +0200

    optimizing

--- others/k_subsequences.cpp
+++ others/k_subsequences.cpp
@@ -7,12 +7,11 @@ public:
 	int kSubsequence(vector<int> nums, int k) {
 		int result = 0;
 		for(auto it1 = nums.begin(); it1 != nums.end(); it1++) {
-			if(*it1 % 5 == 0) result++;
 			int tmp = *it1;
+			if(tmp % 5 == 0) result++;
 			for(auto it2 = it1 + 1; it2 != nums.end(); it2++) {
 				tmp += *it2;
-				if(tmp % 5 == 0)
-					result++;
+				if(tmp % 5 == 0) result++;
 			}
 		}
 		return result;
