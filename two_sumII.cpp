f0f3ddf352abceed59fd37569d95c988a7f50215
songmeo <shuong1012@gmail.com>
2020-03-25 16:55:00 +0200

    attempt 1

--- two_sumII.cpp
+++ two_sumII.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
 	int binarySearch(vector<int> & numbers, int target) {
 		int min, mid, max;
-		min = mid = max = 0;
-		while(min <= max) {
+		min = 0, max = numbers.size() - 1;
+		while(numbers[min] <= numbers[max]) {
 			mid = (min + max)/2;
-			if(target > numbers[mid])
+			if(target < numbers[mid])
 				max = mid - 1;
-			else if(target < numbers[mid])
+			else if(target > numbers[mid])
 				min = mid + 1;
 			else
 				return mid;
@@ -21,12 +22,12 @@ public:
 	}
 	vector<int> twoSum(vector<int>& numbers, int target) {
 		vector<int> result;
-		unordered_map<int, int> m;
-		for(int i = 0; i < (int)numbers.size(); i++) {
-			int pos = binarySearch(numbers, target - numbers[i]);
-			if(pos != -1 && pos != i) {
-				result.push_back(i);
-				result.push_back(pos);
+		for(int i = 0; i < (int)numbers.size() - 1; i++) {
+			vector<int> v(numbers.begin() + i + 1, numbers.end());
+			int pos = binarySearch(v, target - numbers[i]);
+			if(pos != -1) {
+				result.push_back(i + 1);
+				result.push_back(pos + i + 1 + 1);
 				return result;
 			}
 		}
@@ -36,8 +37,8 @@ public:
 
 int main() {
 	Solution s;
-	vector<int> v{2,7,11,15};
-	for(int i : s.twoSum(v, 9)) {
+	vector<int> v{5,25,75};
+	for(int i : s.twoSum(v, 100)) {
 		cout << i << " ";
 	}
 	return 0;
