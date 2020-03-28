66abfa9236edfcd89a068a8760f7c2d141545de8
songmeo <shuong1012@gmail.com>
2020-03-28 11:22:39 +0200

    check negative number

--- reverse_int.cpp
+++ reverse_int.cpp
@@ -6,20 +6,23 @@ using namespace std;
 class Solution {
 public:
 	int reverse(int x) {
-		int result = 0;
-		int tmp = x % 10;
-		int i = 0;
-		while(x) {
-			tmp = x % 10;
-			x /= 10;
-			result += tmp * pow(10,i++);
+		bool negative = false;
+		if(x < 0) {
+			x *= -1;
+			negative = true;
 		}
-		
-		return result;
+		int result = 0;
+		while(x) {
+			int tmp = x % 10;
+			result = result*10 + tmp;
+			x /= 10;
+		}
+		return negative?-1*result:result;
 	}
 };
 
 int main() {
 	Solution s;
-	cout << s.reverse(321);
+	cout << s.reverse(321) << endl;
+	cout << s.reverse(-321) << endl;
 }
