7211a75a383be971c16b7f90dfe5233742cd34ad
songmeo <shuong1012@gmail.com>
2020-04-04 08:49:30 +0300

    brute force

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -5,18 +5,17 @@ using namespace std;
 
 class Solution {
 public:
-	int maxSubArray(vector<int> nums) {
-		int max_sum = INT_MIN;
-		for(auto it1 = nums.begin(); it1 < nums.end(); it1++) {
-			int sum = *it1;
-			for(auto it2 = it1 + 1; it2 < nums.end(); it2++) {
-				if(sum + *it2 < sum) break;
-				sum += *it2;
-			} 
-			if(sum > max_sum) max_sum = sum;
-		}
-		return max_sum;
-	}
+    int maxSubArray(vector<int> nums) {
+        int max_sum = nums[0];
+        for(auto it1 = nums.begin(); it1 < nums.end(); it1++) {
+            int sum = 0;
+            for(auto it2 = it1; it2 < nums.end(); it2++) {
+                sum += *it2;
+                if(sum > max_sum) max_sum = sum;
+            }
+        }
+        return max_sum;
+    }
 };
 
 int main() {
