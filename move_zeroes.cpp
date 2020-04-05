5b85ba0310924e1015838a1c00ef026dafea36ee
songmeo <shuong1012@gmail.com>
2020-04-05 08:54:35 +0300

    add repeat counter

--- move_zeroes.cpp
+++ move_zeroes.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    void moveZeroes(vector<int> nums) {
+    void moveZeroes(vector<int> &nums) {
+		int repeat = 0;
         for(auto it = nums.begin(); it != nums.end();) {
-            if(*it == 0)
-                rotate(it, it + 1, nums.end());
-            else
-                it++;
+            if(*it == 0 && repeat <= (int)nums.size()) {
+				rotate(it, it + 1, nums.end());
+				repeat++;
+			}
+			else 
+				it++;
         }
     }
 };
 
 int main() {
 	Solution s;
-	cout << s.moveZeroes({0,1,0,3,12});
+	vector<int> v = {0,0,1};
+	s.moveZeroes(v);
+	for(int i : v)
+		cout << i;
 	return 0;
 }
