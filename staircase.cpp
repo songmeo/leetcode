8f2a81655d2f7b43c7e2020e31a4d23d64584c41
songmeo <shuong1012@gmail.com>
2020-04-09 19:53:56 +0300

    staircase

---
+++ staircase.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//fibonacci way
+class Solution1 {
+public:
+    int climbStairs(int n) {
+        if(n == 0 || n == 1) return 1;
+        return climbStairs(n - 1) + climbStairs(n-2);
+    }
+};
+
+//bottom up way
+class Solution2 {
+public:
+	int climbStairs(int n) {
+		vector<int> arr(n + 1);
+		arr[0] = 1;
+		arr[1] = 1;
+		for(int i = 2; i <= n; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2];
+		}
+		return arr[n];
+	}
+};
+
+int main() {
+	Solution s;
+	cout << s.climbStairs(44);
+	return 0;
+}
+
