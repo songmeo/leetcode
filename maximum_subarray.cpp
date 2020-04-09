545821a0ac19c4398dc56cbdb898045bc13796cc
songmeo <shuong1012@gmail.com>
2020-04-09 15:19:57 +0300

    kadane algorithm

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <climits>
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
     int maxSubArray(vector<int> nums) {
-        int max_sum = nums[0];
+		list<int> lst;
+        int sum = 0;
         for(auto it1 = nums.begin(); it1 < nums.end(); it1++) {
-            int sum = 0;
-            for(auto it2 = it1; it2 < nums.end(); it2++) {
-                sum += *it2;
-                if(sum > max_sum) max_sum = sum;
-            }
+			lst.push_back(*it1);
+			int tmp = sum;
+			sum += *it1;
+			if(sum < tmp) {
+				sum -= *lst.begin();
+				lst.pop_front();
+			}
         }
-        return max_sum;
+        return sum;
     }
+};
+//using kadane algorithm 
+class Solution {
+public:
+	int maxSubArray(vector<int> nums) {
+		int ans = INT_MIN, tmp = 0;
+		for(int i : nums) {
+			tmp += i;
+			ans = max(ans, tmp);
+			tmp = max(tmp, 0);
+		}
+		return ans;
+	}
 };
 
 int main() {
