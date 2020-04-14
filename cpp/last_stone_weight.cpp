db11bd6937acf03f6b883815b029cda68cfa6d7d
songmeo <shuong1012@gmail.com>
2020-04-14 20:34:15 +0300

    optimizing finding pos

--- cpp/last_stone_weight.cpp
+++ cpp/last_stone_weight.cpp
@@ -5,18 +5,21 @@ using namespace std;
 
 class Solution {
 public:
-    int pos(vector<int> v, int k, int max, int min) {
-        if(v.empty()) return 0;
-        if(max <= min) 
-          return (k > v[min])?(min + 1):min;
-        int mid = (max + min) / 2;
-        if(k == v[mid])
-          return mid;
-        else if(k < v[mid])
-          return pos(v, k, mid - 1, min);
-        else
-          return pos(v, k, max, mid + 1);
-    }
+    int pos(vector<int> v, int k) {
+		if(v.empty()) return 0;
+		int max = v.size() - 1, min = 0, mid = 0;
+		while(max >= min) {
+			mid = (max + min)/2;
+			if(v[mid] == k)
+				return mid;
+			else if(k < v[mid])
+				max = mid - 1;
+			else
+				min = mid + 1;
+		}
+		return (k > v[mid])?mid+1:mid;
+	}
+	
     int lastStoneWeight(vector<int> stones) {
         sort(stones.begin(), stones.end());
         for(auto it = stones.rbegin(); stones.size() > 1 && it != stones.rend();) {
@@ -25,7 +28,7 @@ public:
             stones.pop_back();
             stones.pop_back();
             if(tmp)
-                stones.insert(stones.begin() + pos(stones, tmp, stones.size() - 1, 0), tmp);
+                stones.insert(stones.begin() + pos(stones, tmp), tmp);
             if(stones.size() == 0) return 0;
             it = stones.rbegin();
         }
@@ -37,6 +40,6 @@ public:
 int main() {
 	vector<int> v = {};
 	Solution s;
-	cout << s.lastStoneWeight({3,1});
+	cout << s.lastStoneWeight({2,7,4,1,8,1});
 	return 0;
 }
