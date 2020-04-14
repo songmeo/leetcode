6329c78cf0e88b0d605b7f983da7a5038ebdcdc6
songmeo <shuong1012@gmail.com>
2020-04-14 20:05:45 +0300

    binary search part

--- cpp/last_stone_weight.cpp
+++ cpp/last_stone_weight.cpp
@@ -1,31 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
-    int pos(vector<int> v, int k) {
-        int min = 0, max = v.size() - 1;
-        int mid = 0;
-        while(min <= max) {
-            mid = (min + max)/2;
-            if(v[mid] == k) return mid;
-            else if(v[mid] > k) {
-                max = mid - 1;
-            }
-            else
-                min = mid + 1;
-        }
-        return mid;
+    int pos(vector<int> v, int k, int max, int min) {
+        if(v.empty()) return 0;
+        if(max <= min) 
+          return (k > v[min])?(min + 1):min;
+        int mid = (max + min) / 2;
+        if(k == v[mid])
+          return mid;
+        else if(k < v[mid])
+          return pos(v, k, mid - 1, min);
+        else
+          return pos(v, k, max, mid + 1);
     }
-    int lastStoneWeight(vector<int>& stones) {
+    int lastStoneWeight(vector<int> stones) {
         sort(stones.begin(), stones.end());
-        //1 1 2 4 7 8
-        for(auto it = stones.rbegin(); it != stones.rend() && stones.size() > 1;) {
+        for(auto it = stones.rbegin(); stones.size() > 1 && it != stones.rend();) {
             int first = *it, second = *(it + 1);
             int tmp = abs(first - second);
             stones.pop_back();
             stones.pop_back();
-            cout << pos(stones,tmp);
-            stones.insert(stones.begin() + pos(stones, tmp), tmp);
+            if(tmp)
+                stones.insert(stones.begin() + pos(stones, tmp, stones.size() - 1, 0), tmp);
+            if(stones.size() == 0) return 0;
             it = stones.rbegin();
         }
-        return stones[0];
+        
+        return stones.empty()?0:stones[0];
     }
 };
+
+int main() {
+	vector<int> v = {};
+	Solution s;
+	cout << s.lastStoneWeight({3,1});
+	return 0;
+}
