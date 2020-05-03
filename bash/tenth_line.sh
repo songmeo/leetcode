5bada59166b732a17b3ae439503af870e2106f26
songmeo <shuong1012@gmail.com>
2020-05-03 20:33:58 +0300

    add 2 solutions

--- bash/tenth_line.sh
+++ bash/tenth_line.sh
@@ -1,2 +1,10 @@
 #given a text file file.txt, print just 10th line of the file
+
+#solution 1
 cat file.txt | awk 'NR==10'
+
+#solution 2
+sed -n '10p' file.txt
+
+#solution 3
+awk 'NR==10' file.txt
