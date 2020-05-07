31719ad84c1acb29dc7fab5060cf51b5d59b2b47
songmeo <shuong1012@gmail.com>
2020-05-07 21:37:17 +0300

    initial

---
+++ cpp/cousin_in_binary_tree.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool isCousins(TreeNode* root, int x, int y) {
+	
+	}
+}
