c9b48d132ca7db74ae2e00572acc46768bad25bc
songmeo <shuong1012@gmail.com>
2020-05-11 00:15:58 +0300

    town judge

---
+++ cpp/town_judge.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        
+    }
+};
+
+int main() {
+	Solution s;
+	vector<vector<int>> trust = { {1,2} };
+	cout << s.findJudge(2,trust);
+	return 0;
+}
