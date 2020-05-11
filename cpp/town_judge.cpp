9acb35f652d11b22518950ef0ca0dd7cf2a25060
songmeo <shuong1012@gmail.com>
2020-05-11 08:48:03 +0300

    town judge

--- cpp/town_judge.cpp
+++ cpp/town_judge.cpp
@@ -4,7 +4,17 @@ using namespace std;
 class Solution {
 public:
     int findJudge(int N, vector<vector<int>>& trust) {
-        
+        unordered_map<int,int> in;
+        unordered_map<int,int> out;
+        for(vector<int> t : trust) {
+            in[t[1]]++;
+            out[t[0]]++;
+        }
+        for(int i = 1; i <= N; i++) {
+            if(in[i] == N - 1 && out[i] == 0)
+                return i;
+        }
+        return -1;
     }
 };
 
