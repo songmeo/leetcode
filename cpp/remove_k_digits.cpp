b436d848471a6caa25d38b819be1909c88b26416
songmeo <shuong1012@gmail.com>
2020-05-13 23:17:47 +0300

    remove k digits

---
+++ cpp/remove_k_digits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string removeKdigits(string num, int k) {
+        
+    }
+}
+
+int main() {
+	string num = "102000";
+	int k = 1;
+	return 0;
+}
