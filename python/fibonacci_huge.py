49589c111cad75a7794f7d5c00eb9c1579c08145
songmeo <shuong1012@gmail.com>
2020-06-22 23:24:48 +0300

    algorithm toolbox

---
+++ python/fibonacci_huge.py
@@ -0,0 +1,17 @@
+''' find the last digit of a fibonacci number Fn mod a number m '''
+
+def fib(n):
+	first, second = 0, 1
+	fro
+def period(n, m):
+	for i in r):
+		if(i
+def fibonacci_huge(n, m):
+
+
+if __name__ == '__main__':
+	input = sys.stdin.read();
+    	n, m = map(int, input.split())
+    	print(fibonacci_huge(n, m))
+
+
