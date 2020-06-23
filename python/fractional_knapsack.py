5c397f1d17804051ca5df52b0cff0841f5c983f1
songmeo <shuong1012@gmail.com>
2020-06-23 14:18:37 +0300

    second draft

--- python/fractional_knapsack.py
+++ python/fractional_knapsack.py
@@ -3,14 +3,18 @@ import sys
 
 def get_optimal_value(capacity, weights, values):
     value = 0.
-    value_per_weight = [w/v for w, v in zip(weights, values)]
+    value_per_weight = [v/w for v, w in zip(values, weights)]
     value_per_weight, weights, values = zip(*sorted(zip(value_per_weight, weights, values), reverse=True))
+    i = 0
     for v in value_per_weight:
         n = 0
-        while capacity > n*v and n < values[value_per_weight.index(v)]:
+        while capacity > 0 and n < values[i]:
             n = n + 1
+            capacity = capacity - 1
+        print(f'n {n} ')
         value = value + v*n
-        capacity = capacity - (v/weights[value_per_weight.index(v)])*n
+        i = i + 1
+    for l in [value_per_weight, weights, values]: print(*l)
     return value
 
 
