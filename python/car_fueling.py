50fbdfd539ae39a1b20e1ff7f3a3b22f19d2ad9e
songmeo <shuong1012@gmail.com>
2020-06-24 12:29:11 +0300

    last station could reach destination

--- python/car_fueling.py
+++ python/car_fueling.py
@@ -10,6 +10,8 @@ def compute_min_refills(distance, tank, stops):
     for i in range (2, len(stops)):
         if stops[i] - stops[i - 1] > tank:
             return -1
+    if stops[-1] + tank < distance:
+            return -1
     while curr < distance:
         curr = max([s for s in stops if s < tank + curr])
         curr = curr + tank
