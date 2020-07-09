fd84bc8931db773795495783ea952388f23b2de5
songmeo <shuong1012@gmail.com>
2020-07-09 09:39:25 +0300

    binary search way log(n)

--- python/arrange_coins.py
+++ python/arrange_coins.py
@@ -1,8 +1,28 @@
+#!usr/bin/python3
 class Solution:
-    def arrangeCoins(self, n: int) -> int:
-        i = 0
-        while n >= 0:
-            i = i + 1
-            n -= i
-        return i - 1
+	#naive way
+	def arrangeCoins1(self, n: int) -> int:
+		i = 0
+		while n >= 0:
+			i = i + 1
+			n -= i
+		return i - 1
 
+	#binary search log(n), finding k so that k(k+1)/2 <= n
+	def arrangeCoins(self, n: int) -> int:
+		left, right = 0, n
+		while left <= right:
+			mid = (left + right) // 2
+			cur = mid*(mid + 1) // 2
+			if cur == n:
+				return mid
+			elif cur > n:
+				right = mid - 1
+			else:
+				left = mid + 1
+		return right 
+		
+if __name__ == '__main__':
+	n = int(input())
+	s = Solution()
+	print(s.arrangeCoins(n))
