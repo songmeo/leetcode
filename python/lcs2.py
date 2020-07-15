65b1f0e6aec9b7bc50a1e02b4619cc881332f510
songmeo <shuong1012@gmail.com>
2020-07-15 16:56:01 +0300

    fix when 2 characters are the same

--- python/lcs2.py
+++ python/lcs2.py
@@ -3,12 +3,13 @@ import sys
 def lcs2(a, b):
     #write your code here
     n, m = len(a), len(b)
-    dp = [[0 for i in range(m+1)] for j in range(n+1)]
+    dp = [ [0 for i in range(m + 1)] for j in range(n + 1) ]
     for i in range(1, n + 1):
     	for j in range(1, m + 1):
-    	    dp[i][j] = max(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
-    	    if a[i-1] == b[j-1]:
-    	    	dp[i][j] = dp[i][j] + 1
+    	    if a[i - 1] == b[j - 1]:
+    	    	dp[i][j] = dp[i - 1][j - 1] + 1
+    	    else:
+    	....	dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
     return dp[n][m]
 
 if __name__ == '__main__':
