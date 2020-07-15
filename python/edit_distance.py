38154de09c495e7e2c8064579d1e1787e004f848
songmeo <shuong1012@gmail.com>
2020-07-15 13:27:53 +0300

    fix distance table

--- python/edit_distance.py
+++ python/edit_distance.py
@@ -2,22 +2,22 @@
 def edit_distance(s, t):
     #write your code here
    n, m = len(s), len(t)
-   dist = [[0 for j in range(m)] for i in range(n)]
-   for i in range(n):
+   dist = [[0 for j in range(m+1)] for i in range(n+1)]
+   for i in range(n+1):
    	dist[i][0] = i
-   for j in range(m):
+   for j in range(m+1):
    	dist[0][j] = j
-   for j in range(1,m):
-   	for i in range(1,n):
+   for j in range(1,m+1):
+   	for i in range(1,n+1):
    	    insert = dist[i][j-1] + 1
    	    delete = dist[i-1][j] + 1
    	    match = dist[i-1][j-1]
    	    mismatch = dist[i-1][j-1] + 1
-   	    if s[i] == t[j]:
+   	    if s[i-1] == t[j-1]:
    	        dist[i][j] = min(insert, delete, match)
    	    else:
    	        dist[i][j] = min(insert, delete, mismatch)
-   return dist[n-1][m-1]
+   return dist[n][m]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
