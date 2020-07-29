f76b925dce47a7e3bdae71b91a86a8438d299356
songmeo <shuong1012@gmail.com>
2020-07-29 12:53:42 +0300

    knapsack

---
+++ python/knapsack.py
@@ -0,0 +1,12 @@
+# Uses python3
+import sys
+
+def optimal_weight(W, w):
+    # write your code here
+    result = 0
+    return result
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    W, n, *w = list(map(int, input.split()))
+    print(optimal_weight(W, w))
