8f1a8e12180097e8935be65539cc18fc4732e9a5
songmeo <shuong1012@gmail.com>
2020-08-11 00:34:22 +0300

    valid palindrome

--- python/valid_palindrome.py
+++ python/valid_palindrome.py
@@ -1,4 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = ''.join(filter(str.isalpha, s))
-
+        s = ''.join(filter(str.isalnum, s))
+        if s == '':
+            return True
+        start, end = 0, len(s) - 1
+        while start <= end:
+            if s[start].lower() != s[end].lower():
+                return False
+            start = start + 1
+            end = end - 1
+        return True
